398c05d64df725776d6ca483004caff121939896
Neha Gupta <gneha21@yahoo.in>
2022-09-19 16:37:18 +0530

    fix: add java quickstart page
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>

--- src/components/QuickStart.js
+++ src/components/QuickStart.js
@@ -32,10 +32,10 @@ export const QuickStart = () => {
         </Link>
         <Link
           className=" scale flex flex-col items-center justify-center space-y-3 rounded-lg bg-[color:var(--ifm-card-background-color)] p-6 text-center shadow-lg"
-          to={"https://github.com/keploy/keploy/issues/51"}
+          to={useBaseUrl("/docs/java/run-your-first-app-tutorial")}
         >
           <img className="h-16 w-16" src="/img/java.svg" alt="Java logo" />
-          <p className="text-lg">Java (WIP)</p>
+          <p className="text-lg">Java</p>
         </Link>
       </div>
     </section >
