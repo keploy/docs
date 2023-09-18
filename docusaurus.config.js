3b210dc8d5facdd5f61d1a60328413619f305d14
Vamp <ganeshrevadi16@gmail.com>
2023-09-18 16:29:29 +0530

    chore: add json-ld data and preconnect link (#203)
    Signed-off-by: ganeshrevadi <ganeshrevadi16@gmail.com>
    Signed-off-by: schauhan2 <swati_chauhan@intuit.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,6 +105,31 @@ module.exports = {
           logo: 'https://docs.keploy.io/img/favicon.png',
         }),
       },
+      {name: 'description', content: 'Keploy - API Test Generator Tool'},
+      {name: 'twitter:card', content: 'summary_large_image'},
+    ],
+    headTags: [
+      // Preconnect tag
+      {
+        tagName: 'Keploy Inc',
+        attributes: {
+          rel: 'preconnect',
+          href: 'https://keploy.io/',
+        },
+      },
+      {
+        tagName: 'script',
+        attributes: {
+          type: 'application/ld+json',
+        },
+        innerHTML: JSON.stringify({
+          '@context': 'https://schema.org/',
+          '@type': 'Organization',
+          name: 'Keploy Documentation',
+          url: 'https://docs.keploy.io/',
+          logo: 'https://docs.keploy.io/img/favicon.png',
+        }),
+      },
     ],
     colorMode: {
       defaultMode: "light",
