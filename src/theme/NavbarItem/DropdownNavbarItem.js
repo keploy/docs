05add7723ab36d9d979680e3ced402212df78e99
Akash Kumar <meakash7902@gmail.com>
2023-09-19 10:24:57 +0530

    fix: wrong dropdownItem link
    Signed-off-by: Akash Kumar <meakash7902@gmail.com>

---
+++ src/theme/NavbarItem/DropdownNavbarItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {useLocation} from '@docusaurus/router';
+import DropdownNavbarItem from '@theme-original/NavbarItem/DropdownNavbarItem';
+
+export default function DropdownNavbarItemWrapper(props) {
+  const {search, hash} = useLocation();
+  props.items[0].to = `/docs/server/windows/installation${search}${hash}`;
+  return (
+    <>
+      <DropdownNavbarItem {...props} />
+    </>
+  );
+}
