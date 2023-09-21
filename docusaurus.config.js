99152b16dccaa375ff97d0dd20ee4739b480b6d0
Neha Gupta <gneha21@yahoo.in>
2023-09-21 15:19:29 +0530

    feat: add chat-live (#218)
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>
    Signed-off-by: schauhan2 <swati_chauhan@intuit.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -334,6 +334,11 @@ module.exports = {
       async: true,
       defer: true,
     },
+    {
+      src: "/scripts/chat.js",
+      async: true,
+      defer: true,
+    },
     // {
     //   src: "/scripts/fullstory.js",
     //   async: true,
