34b1293b10ea3761e150a05dd23156b7dad37ac6
Arindam Majumder <arindammajumder2020@gmail.com>
2024-01-26 14:55:29 +0000

    (feat): Added GitTogether Banner.
    Signed-off-by: Arindam Majumder <arindammajumder2020@gmail.com>

---
+++ src/components/GitTogether.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+
+export const GitTogether = () => {
+  return (
+    <section className="mt-1 mb-14">
+      <h2 className="mt-8 text-2xl md:text-3xl font-semibold tracking-wide">
+        Attend GitTogether
+      </h2>
+      <img
+        className="mt-8 "
+        src="/docs/img/GitTogether.jpg"
+        alt={"hacktoberfest-2023"}
+      />
+      <div className="grid grid-cols-1 gap-20 mt-10 md:grid-cols-2">
+        <a
+          className=" scale flex flex-col items-center justify-center space-y-3 rounded-lg bg-[color:orange] p-4 text-center shadow-lg"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSdXYwF3j6AjVrGQn2RWKXqI5awbdmWK7mW2gQYCNAfpqaGZhA/viewform"
+        >
+          <p className="text-lg font-semibold">Register</p>
+        </a>
+        <a
+          className=" scale flex flex-col items-center justify-center space-y-3 rounded-lg bg-[color:orange] p-4 text-center shadow-lg"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSclnnqTRA4x_YhG67eLNOK3LO4-ttqobbMZ5gbUclGNQDvmCg/viewform"
+        >
+          <p className="text-lg font-semibold">Submit CFP</p>
+        </a>
+      </div>
+    </section>
+  );
+};
