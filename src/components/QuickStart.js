99bd006b0df2cfa53c004040c67529711f0a482c
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2024-10-07 21:50:46 +0530

    added Hacktoberfest refernce on the home page
    Signed-off-by: Swapnoneel Saha <swapnoneelsaha111@gmail.com>

--- src/components/QuickStart.js
+++ src/components/QuickStart.js
@@ -28,12 +28,37 @@ export const QuickStart = () => {
       </p>
 
       <h2 className="mt-8 text-2xl font-semibold tracking-wide md:text-3xl">
+        Contribute through Hacktoberfest ✅
+      </h2>
+      <p className="text-l mb-8 mt-4 max-w-4xl">
+        DigitalOcean's Hacktoberfest is a month-long celebration of open-source
+        contributions. Everyone is welcome to participate, regardless of their
+        experience level. Simply make a pull request to an open-source project
+        to get started.
+        <br /> <br />
+        You can contribute to{" "}
+        <a href="https://github.com/orgs/keploy/repositories">
+          several Keploy projects
+        </a>{" "}
+        participating this year, If you’ve ever wanted to contribute to open
+        source , now is your chance!
+        <br /> <br />
+        Anyone around the globe who desires to help drive the growth of open
+        source and make positive contributions to an ever-growing community. All
+        backgrounds and skill levels are encouraged to participate.{" "}
+        <a href="https://opensource.guide/how-to-contribute">
+          Learn How to Contribute?
+        </a>
+      </p>
+
+      <h2 className="mt-8 text-2xl font-semibold tracking-wide md:text-3xl">
         Installation Guide 📗
       </h2>
       <p className="text-l mb-8 mt-4 max-w-4xl">
         Let's get Keploy up and running on your Windows, Linux, or macOS
         machine, so you can start crafting test cases in minutes. ⏱️
       </p>
+
       <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:gap-8">
         <Link
           className="scale flex flex-col items-center justify-center space-y-3 rounded-lg bg-[color:var(--ifm-card-background-color)] p-6 text-center shadow-lg"
