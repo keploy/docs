4efd6592149d519b6521892080792bbbb1f055b1
Inferno-SS5 <sumitsaha.ps@gmail.com>
2025-06-19 12:22:06 +0530

    Redesigned navbar dropdown with improved styling and structure

--- src/theme/NavbarItem/DropdownNavbarItem.js
+++ src/theme/NavbarItem/DropdownNavbarItem.js
@@ -1,13 +1,32 @@
 import React from "react";
-import {useLocation} from "@docusaurus/router";
+import { useLocation } from "@docusaurus/router";
 import DropdownNavbarItem from "@theme-original/NavbarItem/DropdownNavbarItem";
 
 export default function DropdownNavbarItemWrapper(props) {
-  const {search, hash} = useLocation();
+  const { search, hash } = useLocation();
+
+  // Update the first item's link dynamically
   props.items[0].to = `/server/installation/${search}${hash}`;
+
   return (
-    <>
-      <DropdownNavbarItem {...props} />
-    </>
+    <div className="relative group">
+      <DropdownNavbarItem
+        {...props}
+        className="transition duration-300 ease-in-out text-gray-700 hover:text-keploybrightblue"
+      />
+      <div
+        className="absolute left-0 mt-2 w-48 bg-white shadow-lg rounded-xl opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-50"
+      >
+        {props.items.map((item, idx) => (
+          <a
+            key={idx}
+            href={item.to}
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-keployblue hover:text-black"
+          >
+            {item.label}
+          </a>
+        ))}
+      </div>
+    </div>
   );
 }
