0521fd060b447149f0ae2884889d336f26575c47
Tvisha <tvisha.raji@keploy.io>
2025-06-24 00:20:48 +0530

    chore: atg operations (#587)
    Signed-off-by: Lawrence Sharma <che3zcake@gmail.com>

--- src/components/KeployCloud.js
+++ src/components/KeployCloud.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export const KeployCloud = () => {
-
   return (
     <section
       id="cloud"
@@ -10,7 +9,7 @@ export const KeployCloud = () => {
       <div className="max-w-3xl mx-auto prose prose-orange">
         <h1> Question? 🤔💭</h1>
         <p className="my-3 block">
-         For any support please{" "}
+          For any support please{" "}
           <a
             href="https://join.slack.com/t/keploy/shared_invite/zt-357qqm9b5-PbZRVu3Yt2rJIa6ofrwWNg"
             className="text-orange-500 underline hover:text-orange-400"
