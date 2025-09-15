a9a821f9e527140172794aa838215a86817fe8d8
Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>
2025-09-16 03:41:33 +0530

    dark theme issue solved #2986
    Signed-off-by: Azhaan Ali Siddiqui <azhaanalisiddiqui15@gmail.com>

--- src/components/InstallReminder.js
+++ src/components/InstallReminder.js
@@ -3,15 +3,7 @@ import Link from "@docusaurus/Link";
 
 export default function InstallReminder() {
   return (
-    <div
-      style={{
-        padding: "1rem",
-        border: "1px solid #eee",
-        borderRadius: "10px",
-        background: "#fff8f5",
-        margin: "2rem 0",
-      }}
-    >
+    <div className="install-reminder-container">
       <h3>Don’t have Keploy installed yet?</h3>
       <p>
         Before running this sample, make sure Keploy is installed on your
@@ -19,16 +11,7 @@ export default function InstallReminder() {
       </p>
       <Link
         to="/docs/server/installation/"
-        style={{
-          display: "inline-block",
-          marginTop: "0.5rem",
-          padding: "0.6rem 1rem",
-          background: "#e67e22",
-          color: "#fff",
-          borderRadius: "6px",
-          fontWeight: "bold",
-          textDecoration: "none",
-        }}
+        className="install-reminder-button"
       >
         👉 Go to Installation Guide
       </Link>
