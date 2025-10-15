eedf535d21af70e37c5132a913176dae1c2ca2ca
Achanandhi-M <achanandhi.m@gmail.com>
2025-10-15 20:00:26 +0530

    feat: add redis to enterprise quickstart and removed mongo quickstart
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>

--- src/components/EnterpriseInstallReminder.js
+++ src/components/EnterpriseInstallReminder.js
@@ -14,8 +14,8 @@ export default function EnterpriseInstallReminder() {
     >
       <h3>Don’t have Keploy installed yet?</h3>
       <p>
-        Before running this sample, make sure Keploy is installed on your
-        system.
+        Before running this sample, make sure Keploy Enterprise version is
+        installed on your system.
       </p>
       <Link
         to="/docs/keploy-cloud/cloud-installation/"
