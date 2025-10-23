8353c4e30660c730343708317f60bb4a3f82d3e3
Iam-karan-suresh <karansuresh.info@gmail.com>
2025-10-24 03:41:29 +0530

    fix: updated not found page
    Signed-off-by: Iam-karan-suresh <karansuresh.info@gmail.com>

--- src/theme/NotFound/Content/index.js
+++ src/theme/NotFound/Content/index.js
@@ -1,19 +1,28 @@
-import React, {useEffect} from "react";
+import React from "react";
+import {translate} from "@docusaurus/Translate";
+import Link from "@docusaurus/Link";
 import {PageMetadata} from "@docusaurus/theme-common";
 
-export default function Index() {
-  useEffect(() => {
-    window.location.href = "/docs/";
-  }, 2000);
-  [];
-
+export default function NotFound() {
   return (
     <>
-      <PageMetadata title="Redirecting..." />
-      <main className="margin-vert--xl container">
+      <PageMetadata title="Page Not Found" />
+      <main className="container margin-vert--xl">
         <div className="row">
-          <div className="col col--6 col--offset-3">
-            <h1 className="hero__title">Redirecting to home page...</h1>
+          <div className="col col--6 col--offset-3 text-center">
+            <h1 className="hero__title">404: Page Not Found</h1>
+            <p className="padding-vert--md">
+              We could not find what you were looking for.<br />
+              The page you requested may have been moved or deleted.
+            </p>
+            <div className="margin-vert--lg">
+              <Link to="/docs/" className="button button--primary margin-right--md">
+                Back to Homepage
+              </Link>
+              <Link to="/docs/search" className="button button--secondary">
+                Search Documentation
+              </Link>
+            </div>
           </div>
         </div>
       </main>
