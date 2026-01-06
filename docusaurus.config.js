540797f2396c1b235c0c11ac9b4e86d79f0c10d4
Achanandhi M <110651321+Achanandhi-M@users.noreply.github.com>
2026-01-06 14:25:49 +0530

    feat: remove utg commands (#748)
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>
    Signed-off-by: skools-here <sajalkulshreshtha9@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 //@ts-check
 
-import { themes as prismThemes } from "prism-react-renderer";
+import {themes as prismThemes} from "prism-react-renderer";
 const path = require("path");
-import { visit } from "unist-util-visit";
+import {visit} from "unist-util-visit";
 const FontPreloadPlugin = require("webpack-font-preload-plugin");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
@@ -51,7 +51,7 @@ module.exports = {
         content:
           "API testing, Keploy docs, incident replay, network calls, code paths, test scenarios, code coverage, stubs, junit, go-test, live environment, production incidents, open source, regression tests, ai tests",
       },
-      { name: "twitter:card", content: "summary_large_image" },
+      {name: "twitter:card", content: "summary_large_image"},
     ],
     headTags: [
       // Preconnect tag
