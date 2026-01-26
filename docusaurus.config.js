a9cf3323f0ff66af9712c6bbf2d744ecfa86fc00
Neha Gupta <gneha21@yahoo.in>
2026-01-26 22:24:47 +0530

    feat: add v4
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -264,7 +264,7 @@ module.exports = {
            * in `/docs/next` directory, only versioned docs.
            */
           // excludeNextVersionDocs: false,
-          lastVersion: "3.0.0",
+          lastVersion: "4.0.0",
           versions: {
             "1.0.0": {
               label: "1.0.0",
@@ -276,8 +276,13 @@ module.exports = {
               path: "2.0.0",
               banner: "unmaintained",
             },
+            "3.0.0": {
+              label: "3.0.0",
+              path: "3.0.0",
+              banner: "unmaintained",
+            },
           },
-          onlyIncludeVersions: ["1.0.0", "2.0.0", "3.0.0"],
+          onlyIncludeVersions: ["1.0.0", "2.0.0", "3.0.0", "4.0.0"],
           includeCurrentVersion: true, // excludeNextVersionDocs is now deprecated
           // // below remark plugin disabled until we can figure out why it is not transpiling to ESNext properly - swyx
           remarkPlugins: [
