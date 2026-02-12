951508476c9fc969a9e0195cbcb729832b351a5d
TvisharajiK <tvisha.raji@keploy.io>
2026-02-12 19:18:24 +0530

    chatbot

---
+++ static/scripts/chatwoot.js
@@ -0,0 +1,13 @@
+(function(d,t) {
+  var BASE_URL="https://chatwoot.keploy.io";
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src=BASE_URL+"/packs/js/sdk.js";
+  g.async = true;
+  s.parentNode.insertBefore(g,s);
+  g.onload=function(){
+    window.chatwootSDK.run({
+      websiteToken: 'DNsHCafpdxqz3dDU1SPggAon',
+      baseUrl: BASE_URL
+    })
+  }
+})(document,"script");
