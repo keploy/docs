8091a3f0a6a04d15a0aeefe35b424e263356e1d9
amaan-bhati <amaanbhati49@gmail.com>
2026-05-15 06:46:07 +0530

    chore: turn off announcement bar after gittogether
    Signed-off-by: amaan-bhati <amaanbhati49@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -222,11 +222,11 @@ module.exports = {
       //   },
       // },
     },
-    announcementBar: {
-      id: "announcementBar_gittogether_sf_2026", // Increment on change to reset dismissed state
-      content: "GitTogether SF • May 14, 2026 • San Francisco",
-      isCloseable: true,
-    },
+    // announcementBar: {
+    //   id: "announcementBar_gittogether_sf_2026", // Increment on change to reset dismissed state
+    //   content: "GitTogether SF • May 14, 2026 • San Francisco",
+    //   isCloseable: true,
+    // },
     prism: {
       theme: prismThemes.vsLight,
       darkTheme: prismThemes.dracula,
